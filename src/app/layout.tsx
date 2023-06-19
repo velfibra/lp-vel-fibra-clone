7afc213bf3638ce5a77d59f7e1754e42304264ec
Breno Trindade <brenokaminski@hotmail.com>
2023-06-19 11:49:46 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Click Speed – Tenha todo o poder da internet 100% Fibra Óptica',
+  description: 'Navegue com velocidade máxima e confiabilidade coma o a internet da Click Speed. Internet Click Speed 100% Fibra Óptica para você navegar à vontade. Ultra velocidade de 600mb.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br" className='scroll-smooth'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/templates/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+
+export default function Home() {
+    return(
+        <div>
+            <Header />
+            <main>
+                teste
+            </main>
+        </div>
+    )
+}
