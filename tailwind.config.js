81d979f5973f30c0aa1333b26a1a64db7e512a50
Breno Trindade <brenokaminski@hotmail.com>
2023-06-19 15:56:09 -0300

    add: animations for contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/templates/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -10,7 +10,74 @@ module.exports = {
       colors: {
         primary: '#511E84',
       },
+      textShadow: {
+        DEFAULT: '0.1em 0.1em 0.3em var(--tw-shadow-color, black)',
+      },
+      animationDelay: {
+        0: '0',
+        75: '75ms',
+        100: '100ms',
+        150: '150ms',
+        200: '200ms',
+        300: '300ms',
+        500: '500ms',
+        700: '700ms',
+        1000: '1000ms',
+      },
+      keyframes: {
+        fadeInUp: {
+          from: { opacity: 0, transform: 'translateY(100%)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeInRight: {
+          from: { opacity: 0, transform: 'translateX(100%)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        fadeInDown: {
+          from: { opacity: 0, transform: 'translateY(-100%)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        fadeInLeft: {
+          from: { opacity: 0, transform: 'translateX(-100%)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        fadeIn: {
+          from: { opacity: 0.4 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 1s both',
+        fadeInRight: 'fadeInRight 1s both',
+        fadeInDown: 'fadeInDown 1s both',
+        fadeInLeft: 'fadeInLeft 1s both',
+        fadeIn: 'fadeIn 1.5s both',
+      },
+      backgroundImage: {
+        'hero-image': "url('/images/hero.jpg')",
+        'hero-image-mobile': "url('/images/hero-mobile.jpg')",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') },
+      );
+
+      matchUtilities(
+        {
+          'animation-delay': (value) => ({
+            animationDelay: value,
+          }),
+        },
+        { values: theme('animationDelay') },
+      );
+    },
+  ],
 };
