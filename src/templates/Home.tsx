19efcb3ab873c1581c70dd74775d188b19639b7d
Breno Trindade <brenokaminski@hotmail.com>
2023-06-19 15:58:17 -0300

    feat: calling in home the hero test

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,12 +1,13 @@
-import Header from "@/components/Header"
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 export default function Home() {
-    return(
-        <div>
-            <Header />
-            <main>
-                teste
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
 }
