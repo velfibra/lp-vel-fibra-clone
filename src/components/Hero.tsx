6d580ca54ed3f33a4c35d88e201f84cec67d47ca
Breno Trindade <brenokaminski@hotmail.com>
2023-06-20 16:55:32 -0300

    style: mobile refact hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
-    <div className="flex h-[800px] flex-col bg-gray-500 max-lg:h-[450px]">
-      <div className="ml-20 mt-20 h-fit text-7xl max-lg:ml-2 max-lg:mt-5 max-lg:text-2xl">
+    <div className="flex h-[800px] flex-col bg-gray-500 max-lg:h-[500px]">
+      <div className="ml-20 mt-20 h-fit text-7xl max-lg:ml-2 max-lg:mt-10 max-lg:text-2xl">
         <div className="animate-fadeInLeft animation-delay-300">
           <h1 className="text-white">Cansado de ficar</h1>
           <h1 className="mb-3 mt-4 w-fit rounded-xl bg-white p-2 uppercase text-gray-500">sem</h1>
@@ -15,7 +15,7 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <h3 className="mt-44 animate-fadeInUp text-center text-3xl font-bold text-white animation-delay-1000 max-lg:mt-16 max-lg:text-lg">
+      <h3 className="mt-44 animate-fadeInUp text-center text-3xl font-bold text-white animation-delay-1000 max-lg:mt-24 max-lg:text-lg">
         VIVA MUITO MAIS CONECTADO
       </h3>
     </div>
