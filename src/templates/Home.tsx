582f1793e8a27943584ad235561f36e15bd20a6c
Breno Trindade <brenokaminski@hotmail.com>
2023-06-20 12:06:17 -0300

    feat: calling plansContent

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import PlansContent from '@/components/PlansContent';
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <PlansContent />
       </main>
     </div>
   );
