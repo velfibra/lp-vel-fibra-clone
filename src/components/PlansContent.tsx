ea9404e0ab9dfc771ac2219214e67578cbf3bad1
Breno Trindade <brenokaminski@hotmail.com>
2023-06-20 17:51:48 -0300

    feat: offercard div

--- src/components/PlansContent.tsx
+++ src/components/PlansContent.tsx
@@ -15,8 +15,13 @@ export default function PlansContent() {
         </h3>
       </div>
       <div className="grid grid-cols-3 gap-5 max-lg:grid-cols-1">
-        {plans.map(({ h1, h2, text, bg, hover, icons }) => (
+        {plans.map(({ h1, h2, text, bg, hover, icons, offer }) => (
           <Card className={`flex flex-col gap-3 py-20 ${text}`} key={h1}>
+            {offer && (
+              <div className="absolute -mt-24 ml-24 w-[200px] animate-bounce rounded-xl border-[1px] border-black bg-primary p-3 px-5 text-center text-xl text-white">
+                Melhor oferta
+              </div>
+            )}
             <CardTitle>{h1}</CardTitle>
             <h2 className="mb-5 w-full text-center text-5xl font-bold max-lg:text-4xl">{h2}</h2>
             <CardContent>
@@ -56,6 +61,7 @@ const plans = [
     h1: '450 MB',
     h2: 'R$119,90/Mês',
     text: 'text-secondary',
+    offer: true,
     bg: 'from-primary to-primary/60',
     hover: 'hover:bg-gradient-to-b hover:from-secondary hover:to-secondary/60',
     icons: [{ src: '/4.png' }, { src: '/3.png' }, { src: '/5.png' }, { src: '/2.png' }],
