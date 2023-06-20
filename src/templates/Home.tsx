2415e81390f23dccd818457a18584e854e0c8313
Breno Trindade <brenokaminski@hotmail.com>
2023-06-20 15:30:38 -0300

    feat: calling location content in home

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import LocationContent from '@/components/LocationContent';
 import PlansContent from '@/components/PlansContent';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <Hero />
         <PlansContent />
+        <LocationContent />
       </main>
     </div>
   );
