84b69e8ba19a4aed3ff1fc638501ec77242637c8
Breno Trindade <brenokaminski@hotmail.com>
2023-06-21 11:26:11 -0300

    add: help svg

---
+++ src/components/HelpSvg.tsx
@@ -0,0 +1,43 @@
+export default function HelpSvg() {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="250px"
+      height="250px"
+      viewBox="0,0,256,256"
+    >
+      <g
+        fill="none"
+        fill-rule="nonzero"
+        stroke="none"
+        stroke-width="1"
+        stroke-linecap="butt"
+        stroke-linejoin="miter"
+        stroke-miterlimit="10"
+        stroke-dasharray=""
+        stroke-dashoffset="0"
+        font-family="none"
+        font-weight="none"
+        font-size="none"
+        text-anchor="none"
+      >
+        <g transform="scale(3.2,3.2)">
+          <path
+            d="M40,77.5c-20.7,0 -37.5,-16.8 -37.5,-37.5c0,-20.7 16.8,-37.5 37.5,-37.5c20.7,0 37.5,16.8 37.5,37.5c0,20.7 -16.8,37.5 -37.5,37.5z"
+            fill="#511e84"
+          ></path>
+          <path
+            d="M40,3c20.4,0 37,16.6 37,37c0,20.4 -16.6,37 -37,37c-20.4,0 -37,-16.6 -37,-37c0,-20.4 16.6,-37 37,-37M40,2c-21,0 -38,17 -38,38c0,21 17,38 38,38c21,0 38,-17 38,-38c0,-21 -17,-38 -38,-38z"
+            fill="#e40046"
+          ></path>
+          <path
+            d="M40,57c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3zM42,51h-4v-3c0,-4.9 2.6,-7.5 4.9,-9.9c2.2,-2.2 4.1,-4.1 4.1,-8.1c0,-3.9 -3.1,-7 -7,-7c-3.9,0 -7,3.1 -7,7v4h-4v-4c0,-6.1 4.9,-11 11,-11c6.1,0 11,4.9 11,11c0,5.6 -2.9,8.6 -5.3,11c-2.1,2.1 -3.7,3.8 -3.7,7z"
+            fill="#ffffff"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
