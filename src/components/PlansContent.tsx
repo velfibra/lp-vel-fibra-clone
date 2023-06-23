738dd64465f7f4bf88748669091bff8c8dc422e4
Breno Trindade <brenokaminski@hotmail.com>
2023-06-23 09:30:50 -0300

    feat: function open modal

--- src/components/PlansContent.tsx
+++ src/components/PlansContent.tsx
@@ -1,9 +1,26 @@
+'use client';
+import { useState } from 'react';
 import Card from './Card';
 import CardContent from './CardContent';
 import CardTitle from './CardTitle';
 import Image from 'next/image';
+import Modal from './Modal';
+import Form from './TestForm';
 
 export default function PlansContent() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [priceItem, setPriceItem] = useState('');
+  const [title, setTitle] = useState('');
+
+  const openModal = (price: string, h1: string) => {
+    setIsModalOpen(true);
+    setPriceItem(price);
+    setTitle(h1);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <section className="mx-auto mb-10 mt-5 max-w-[1580px] p-6 max-lg:w-full">
       <div className="mb-10">
@@ -18,33 +35,37 @@ export default function PlansContent() {
         </h3>
       </div>
       <div className="grid grid-cols-3 gap-5 max-lg:grid-cols-1">
-        {plans.map(({ h1, h2, text, bg, hover, icons, offer }) => (
-          <Card className={`rise flex animate-fadeInUp flex-col gap-3 py-20 ${text}`} key={h1}>
-            {offer && (
-              <div className="absolute -mt-24 w-[220px] animate-bounce rounded-xl border-[1px] border-black bg-primary p-3 px-5 text-center text-xl text-white max-lg:ml-[31px] xl:ml-[75px] 2xl:ml-[110px]">
-                Melhor oferta
-              </div>
-            )}
-            <CardTitle>{h1}</CardTitle>
-            <h2 className="mb-5 w-full text-center text-5xl font-bold max-lg:text-4xl">{h2}</h2>
-            <CardContent>
-              <h3 className="text-center text-xl font-semibold">Instalação gratuita</h3>
-              <hr className="mt-5 h-[2px] bg-black/40" />
-              <h3 className="text-center text-xl font-semibold">Wi-fi Incluso</h3>
-              <hr className="h-[2px] bg-black/40" />
-              <div className="mt-10 flex justify-center gap-2">
-                {icons?.map(({ src }) => (
-                  <Image key={src} src={src} alt="Beneficio Click" width={40} height={40} />
-                ))}
-              </div>
-              <h3 className="my-10 text-center text-xl font-semibold">*Valor com pontualidade</h3>
-            </CardContent>
-            <button
-              className={`h-14 w-[70%] self-center rounded-lg bg-gradient-to-b max-lg:w-[90%] ${bg} py-2 text-xl font-bold uppercase text-white shadow-md shadow-gray-500 duration-500 hover:scale-105 ${hover}`}
-            >
-              contratar
-            </button>
-          </Card>
+        {plans.map(({ h1, h2, price, text, bg, hover, icons, offer }) => (
+          <div key={h1}>
+            <Card className={`rise flex animate-fadeInUp flex-col gap-3 py-20 ${text}`}>
+              {offer && (
+                <div className="absolute -mt-24 w-[220px] animate-bounce rounded-xl border-[1px] border-black bg-primary p-3 px-5 text-center text-xl text-white max-lg:ml-[31px] xl:ml-[75px] 2xl:ml-[110px]">
+                  Melhor oferta
+                </div>
+              )}
+              <CardTitle>{h1}</CardTitle>
+              <h2 className="mb-5 w-full text-center text-5xl font-bold max-lg:text-4xl">{h2}</h2>
+              <CardContent>
+                <h3 className="text-center text-xl font-semibold">Instalação gratuita</h3>
+                <hr className="mt-5 h-[2px] bg-black/40" />
+                <h3 className="text-center text-xl font-semibold">Wi-fi Incluso</h3>
+                <hr className="h-[2px] bg-black/40" />
+                <div className="mt-10 flex justify-center gap-2">
+                  {icons?.map(({ src }) => (
+                    <Image key={src} src={src} alt="Beneficio Click" width={40} height={40} />
+                  ))}
+                </div>
+                <h3 className="my-10 text-center text-xl font-semibold">*Valor com pontualidade</h3>
+              </CardContent>
+              <button
+                onClick={() => openModal(price, h1)}
+                className={`h-14 w-[70%] self-center rounded-lg bg-gradient-to-b max-lg:w-[90%] ${bg} py-2 text-xl font-bold uppercase text-white shadow-md shadow-gray-500 duration-500 hover:scale-105 ${hover}`}
+              >
+                contratar
+              </button>
+            </Card>
+            <Modal price={priceItem} h1={title} isOpen={isModalOpen} onClose={closeModal} />
+          </div>
         ))}
       </div>
     </section>
@@ -55,6 +76,7 @@ const plans = [
   {
     h1: '350 MB',
     h2: 'R$99,90/Mês',
+    price: '99,90',
     text: 'text-primary',
     bg: 'from-secondary to-secondary/60',
     hover: 'hover:bg-gradient-to-b hover:from-primary hover:to-primary/60',
@@ -63,6 +85,7 @@ const plans = [
   {
     h1: '450 MB',
     h2: 'R$119,90/Mês',
+    price: '119,90',
     text: 'text-secondary',
     offer: true,
     bg: 'from-primary to-primary/60',
@@ -72,6 +95,7 @@ const plans = [
   {
     h1: '650 MB',
     h2: 'R$139,90/Mês',
+    price: '139,90',
     text: 'text-primary',
     bg: 'from-secondary to-secondary/60',
     hover: 'hover:bg-gradient-to-b hover:from-primary hover:to-primary/60',
