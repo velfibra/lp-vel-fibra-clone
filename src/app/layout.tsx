2d72c3d40e2072c1f7122d1f0e20fab74805638e
Breno Trindade <brenokaminski@hotmail.com>
2023-06-23 10:42:33 -0300

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata = {
   title: 'Click Speed – Tenha todo o poder da internet 100% Fibra Óptica',
   description:
     'Navegue com velocidade máxima e confiabilidade coma o a internet da Click Speed. Internet Click Speed 100% Fibra Óptica para você navegar à vontade. Ultra velocidade de 600mb.',
+  icons: [{ url: '/favicon.ico', sizes: 'any' }],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
