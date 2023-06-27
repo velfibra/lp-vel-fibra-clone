94a5263de9515048d441423d64f57c9513db2d24
Breno Trindade <brenokaminski@hotmail.com>
2023-06-27 16:35:44 -0300

    feat: text for cookies

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -29,29 +29,24 @@ export default function CookieBanner() {
     <div
       className={`fixed bottom-0 left-0 right-0
                         mx-auto my-10  ${cookieConsent != null ? 'hidden' : 'flex'} max-w-max 
-                        flex-col items-center justify-between gap-4 rounded-lg bg-gray-700 px-3 py-3 shadow  
+                        flex-col items-center justify-between gap-4 rounded-lg bg-gray-300 px-3 py-3 shadow  
                          sm:flex-row md:max-w-screen-sm md:px-4`}
     >
       <div className="text-center">
         <Link href="/info/cookies">
-          <p className="text-white">
-            Nós usamos <span className="font-bold text-sky-400">cookies</span> em nosso site.
+          <p className="text-gray-800">
+            Utilizamos<span className="font-bold text-sky-400">cookies</span> para garantir a melhor
+            experiência em nosso site.
           </p>
         </Link>
       </div>
 
       <div className="flex gap-2">
         <button
-          className="rounded-md border-gray-900 px-5 py-2 text-gray-300"
-          onClick={() => setCookieConsent(false)}
-        >
-          Negar
-        </button>
-        <button
           className="rounded-lg bg-gray-900 px-5 py-2 text-white"
           onClick={() => setCookieConsent(true)}
         >
-          Permitir
+          Aceitar
         </button>
       </div>
     </div>
