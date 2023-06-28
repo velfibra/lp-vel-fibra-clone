59bf4213fb24f3c9dfb739cccbc28091930e0ffe
Breno Trindade <brenokaminski@hotmail.com>
2023-06-28 16:53:08 -0300

    feat: image location incomplete

--- src/components/LocationContent.tsx
+++ src/components/LocationContent.tsx
@@ -1,11 +1,11 @@
 export default function LocationContent() {
   return (
-    <section className="flex flex-col bg-local-image bg-cover max-lg:h-[580px] max-lg:justify-end xl:h-[600px] 2xl:h-[800px]">
+    <section className="flex flex-col bg-local-image bg-cover max-lg:h-[580px] max-lg:justify-end xl:h-[500px] 2xl:h-[600px]">
       <div className="flex flex-col max-lg:mb-5">
-        <h1 className="rise mb-5 mr-20 animate-fadeInRight text-end font-bold uppercase text-white text-shadow animation-delay-150 max-lg:mr-0 max-lg:text-center max-lg:text-3xl xl:text-[80px] 2xl:text-[130px]">
-          estamos aqui!
+        <h1 className="rise mb-5 mr-20 animate-fadeInRight text-end font-bold uppercase text-white text-shadow animation-delay-150 max-lg:mr-0 max-lg:text-center max-lg:text-3xl xl:text-[35px] 2xl:text-[50px]">
+          nossa área de cobertura
         </h1>
-        <div className="rise ml-auto mr-14 flex animate-fadeInRight justify-end font-semibold  text-white text-shadow animation-delay-150 max-lg:mr-0 max-lg:w-full max-lg:justify-center max-lg:text-[12px] xl:text-lg 2xl:text-xl">
+        <div className="rise ml-auto mr-14 mt-10 flex animate-fadeInRight justify-end font-semibold text-white text-shadow animation-delay-150 max-lg:mr-0 max-lg:w-full max-lg:justify-center max-lg:text-[12px] xl:text-base 2xl:text-xl">
           {locations.map(({ local }, i) => (
             <ul
               className="ml-5 h-fit w-fit border-x-[1px] px-2 first:border-none last:border-none max-lg:ml-3 max-lg:px-0"
