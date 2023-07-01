ced8ba560101ffad513168f12ffbc06267b3df40
Click-Speed <development@clickspeed.net.br>
2023-07-01 10:00:45 -0300

    test: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import Rise from '@/components/Rise';
 import { Inter } from 'next/font/google';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 import CookieBanner from '@/components/CookieBanner';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 //
@@ -22,8 +24,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           content="pVoqKhYcW4rD9ZFppSPc0Dq6IUPZ5KqziBXFcVzcTGA"
         />
       </head>
+      <div className="container">
+        <Script id="google-analytics">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WHX92B2');
+        `}
+        </Script>
+      </div>
       <GoogleAnalytics GA_MEASUREMENT_ID="G-V2149R76YL" />
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WHX92B2"
+            height="0"
+            width="0"
+            className="hidden"
+          ></iframe>
+        </noscript>
         {children}
         <CookieBanner />
         <Rise />
