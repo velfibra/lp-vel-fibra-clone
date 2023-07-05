429b268b5af0ff93d03b2701eeb86cb8d8bebada
Click-Speed <development@clickspeed.net.br>
2023-07-05 14:17:22 -0300

    feat: title for awardContent

--- src/components/AwardContent.tsx
+++ src/components/AwardContent.tsx
@@ -2,10 +2,18 @@ import Image from 'next/image';
 
 export default function AwardContent() {
   return (
-    <section className="mx-auto my-10 grid w-1/2 grid-cols-3 gap-8 max-lg:-mt-5 max-lg:grid-cols-1">
-      {awards.map(({ src, alt }) => (
-        <Image key={alt} src={src} alt={alt} width={300} height={300} />
-      ))}
+    <section>
+      <h1
+        id="help"
+        className="text-center text-3xl font-bold uppercase text-primary max-lg:w-[95%] max-lg:text-2xl"
+      >
+        A Click Speed é o provedor de internet mais premiado de Belém!
+      </h1>
+      <div className="mx-auto my-10 grid w-1/2 grid-cols-3 gap-8 max-lg:grid-cols-1">
+        {awards.map(({ src, alt }) => (
+          <Image key={alt} src={src} alt={alt} width={300} height={300} />
+        ))}
+      </div>
     </section>
   );
 }
