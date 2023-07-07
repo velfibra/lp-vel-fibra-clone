98ea35f244a4c29e27088d104a7752e5cf3a46e1
Click-Speed <development@clickspeed.net.br>
2023-07-07 15:23:28 -0300

    feat: comment whatsapp button

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <WhatsAppButton />
+      {/* <WhatsAppButton /> */}
       <main>
         <Hero />
         <PlansContent />
