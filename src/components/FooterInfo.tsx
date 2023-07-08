b77c0d425ad839bb626cfb8112b928a200746923
Click-Speed <development@clickspeed.net.br>
2023-07-08 10:29:01 -0300

    refact: ratio image

--- src/components/FooterInfo.tsx
+++ src/components/FooterInfo.tsx
@@ -5,11 +5,11 @@ export default function FooterInfo() {
     <>
       <div className="-mb-3 flex flex-col items-center max-lg:w-full max-md:-ml-5">
         <Image
-          className="h-36 w-48 max-lg:ml-6 xl:-ml-10"
+          className="h-40 w-64 max-lg:ml-6 xl:-ml-10"
           src={'/CLICK SPEED VERTICAL@4x 1.png'}
           alt="clickspeed logo white"
-          width={200}
-          height={200}
+          width={250}
+          height={167}
         />
         <p className="w-60 font-semibold max-lg:ml-10 max-lg:text-center max-md:w-full">
           A internet que se garante.
