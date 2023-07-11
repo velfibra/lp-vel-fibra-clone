a5d143698f544d1e7ec9d543c16044d20d68dc7e
Click-Speed <development@clickspeed.net.br>
2023-07-11 11:37:43 -0300

    feat: redirect for whatsapp seller

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -1,35 +1,18 @@
-'use client';
-import { useState } from 'react';
 import Modal from './Modal';
 import WhatsAppIcon from './icons/WhatsApp';
 
+const phoneNumber = 559130853190;
+
 export default function WhatsAppButton() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
   return (
-    <>
-      <button
-        onClick={() => openModal()}
-        className="hover: fixed bottom-3 right-0.5 z-50 select-none text-[#40c351] duration-200 hover:scale-125 hover:text-[#29ff45]"
-        aria-label="WhatsApp button"
-        title="WhatsApp"
-      >
-        <WhatsAppIcon />
-      </button>
-      <Modal
-        price={'99,90'}
-        h1={'350 MB'}
-        id="form_350mb"
-        isOpen={isModalOpen}
-        onClose={closeModal}
-      />
-    </>
+    <a
+      href={`https://wa.me/${phoneNumber}`}
+      className="hover: fixed bottom-3 right-0.5 z-50 text-[#40c351] duration-200 hover:scale-125 hover:text-[#29ff45]"
+      target="_blank"
+      aria-label="WhatsApp button"
+      title="WhatsApp"
+    >
+      <WhatsAppIcon />
+    </a>
   );
 }
