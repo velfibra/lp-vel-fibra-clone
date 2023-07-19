c3715ef3f45acb4f386669f954c2584e71b61bc4
Click-Speed <development@clickspeed.net.br>
2023-07-19 15:19:25 -0300

    add: maps

--- src/components/LocationContent.tsx
+++ src/components/LocationContent.tsx
@@ -1,83 +1,12 @@
 export default function LocationContent() {
   return (
-    <section className="flex flex-col bg-local-image bg-cover max-lg:h-[580px] max-lg:bg-local-image-mobile xl:h-[500px] 2xl:h-[600px]">
-      <div className="flex flex-col max-lg:mb-5">
-        <h1
-          id="coverage"
-          className="rise mb-5 animate-fadeInUp text-end font-bold uppercase text-white text-shadow animation-delay-150 max-lg:mr-0 max-lg:animate-fadeInLeft max-lg:text-center max-lg:text-3xl max-lg:text-[20px] max-lg:animation-delay-500 xl:mr-5 xl:text-[35px] 2xl:mr-20 2xl:text-[50px]"
-        >
-          nossa área de cobertura
-        </h1>
-        <div className="rise xl>mr-5 ml-auto flex animate-fadeInUp justify-end font-semibold text-white text-shadow animation-delay-150 max-lg:mr-0 max-lg:w-full max-lg:animate-fadeInLeft max-lg:justify-center max-lg:text-[12px] max-lg:animation-delay-500 xl:mt-10 xl:text-sm 2xl:mr-14 2xl:text-xl">
-          {locations.map(({ local }, i) => (
-            <ul
-              className="ml-5 h-fit w-fit border-x-[1px] px-2 first:border-none last:border-none max-lg:ml-3 max-lg:px-0"
-              key={i}
-            >
-              {local.map((i) => (
-                <li key={i}>{i}</li>
-              ))}
-            </ul>
-          ))}
-        </div>
-      </div>
+    <section className="max-lg:h-[580px] xl:h-[500px] 2xl:h-[600px]">
+      <iframe
+        className="mx-auto rounded-lg shadow-md shadow-gray-700"
+        src="https://www.google.com/maps/d/u/0/embed?mid=1dGIU8AyDYq2LeRQs6hw4ajel6ant_d0&ehbc=2E312F"
+        width="80%"
+        height="90%"
+      ></iframe>
     </section>
   );
 }
-
-const locations = [
-  {
-    local: [
-      '-GUAMÁ',
-      '-TERRA FIRME',
-      '-JURUNAS',
-      '-PEDREIRA',
-      '-CANUDOS',
-      '-BARREIRO',
-      '-CONDOR',
-      '-CREMEÇÃO',
-      '-CURIÓ UTINGA',
-      '-MARCO',
-      '-BENGUI',
-      '-SIDERAL',
-      '-MANGUEIRÃO',
-      '-CATALINA',
-    ],
-  },
-  {
-    local: [
-      '-PARQUE VERDE',
-      '-PRATINHA',
-      '-MARACANGALHA',
-      '-TAPANÃ',
-      '-TENONÉ',
-      '-MARACAUERA',
-      '-COQUEIRO',
-      '-40 HORAS',
-      '-ICUÍ',
-      '-MAGUARI-CAJUÍ',
-      '-PARQUE GUAJARÁ',
-      '-ÁGUAS NEGRAS',
-      '-SÃO JOÃO',
-      '(RESIDENCIAL',
-      ' BEM VIVER)',
-    ],
-  },
-  {
-    local: [
-      '-SÃO CLEMENTE',
-      '-CIDADE NOVA 6',
-      '-GUAJARÁ',
-      '-BATISTA CAMPOS',
-      '-CIDADE VELHA',
-      '-SÃO BRÁS',
-      '-VAL-DE-CÃES',
-      '-SACRAMENTA',
-      '-MARAMBAIA',
-      '-TELEGRAFO',
-      '-JADERLÂDIA',
-      '-CIDADE NOVA V',
-      '-CAMPINA',
-    ],
-  },
-];
