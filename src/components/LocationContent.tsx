d2d3676b8bb63f233293852f7b0c0284cbd2f8b5
Click-Speed <development@clickspeed.net.br>
2023-07-21 09:14:04 -0300

    style: map

--- src/components/LocationContent.tsx
+++ src/components/LocationContent.tsx
@@ -1,17 +1,14 @@
 export default function LocationContent() {
   return (
-    <section className="rise animate-fadeInUp max-lg:h-[580px] xl:h-[500px] 2xl:h-[600px]">
-      <h1
-        id="plan"
-        className="rise mb-5 animate-fadeInUp text-center text-3xl font-bold uppercase text-primary max-lg:text-2xl"
-      >
+    <section className="rise animate-fadeInUp max-lg:h-[580px] xl:mx-auto xl:h-[500px] xl:w-[80%] 2xl:h-[600px]">
+      <h1 className="rise mb-5 animate-fadeInUp text-center text-3xl font-bold uppercase text-primary max-lg:text-2xl">
         Estamos por toda Belém!
       </h1>
       <iframe
-        className="mx-auto rounded-lg shadow-md shadow-gray-700"
+        className="mx-auto shadow-gray-700 xl:rounded-lg xl:shadow-md"
         src="https://www.google.com/maps/d/u/0/embed?mid=1dGIU8AyDYq2LeRQs6hw4ajel6ant_d0&ehbc=2E312F"
-        width="80%"
-        height="90%"
+        width="100%"
+        height="100%"
       ></iframe>
     </section>
   );
