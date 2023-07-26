ef478d4a3d754922e57fccd39c85f720a1968721
Click-Speed <development@clickspeed.net.br>
2023-07-26 14:22:15 -0300

    fix: error in animation

--- src/components/AwardContent.tsx
+++ src/components/AwardContent.tsx
@@ -22,18 +22,15 @@ export default function AwardContent() {
           </h1>
           <div className="mx-auto mt-2 flex flex-col">
             {awards.map(({ src, alt, description }) => (
-              <div
-                className="rise flex animate-fadeInRight items-center gap-5 max-lg:w-[90%] xl:my-2 2xl:my-5"
-                key={alt}
-              >
+              <div className="flex items-center gap-5 max-lg:w-[90%] xl:my-2 2xl:my-5" key={alt}>
                 <Image
-                  className="max-lg:w-[70px] xl:h-[110px] xl:w-[80px] 2xl:h-[100] 2xl:w-[100]"
+                  className="rise animate-fadeInUp max-lg:w-[70px] xl:h-[110px] xl:w-[80px] 2xl:h-[100] 2xl:w-[100]"
                   src={src}
                   alt={alt}
                   width={100}
                   height={194}
                 />
-                <h2 className="-mr-14 w-full text-left font-semibold max-lg:text-lg xl:-mr-28 xl:text-xl 2xl:text-2xl">
+                <h2 className=" rise -mr-14 w-full animate-fadeIn text-left font-semibold animation-delay-500 max-lg:text-lg xl:-mr-28 xl:text-xl 2xl:text-2xl">
                   {description}
                 </h2>
               </div>
