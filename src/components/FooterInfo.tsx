03a16e09d47e8b7177ebe405759f153f7681242e
Click-Speed <development@clickspeed.net.br>
2023-07-27 08:51:28 -0300

    add: logo white for footer

--- src/components/FooterInfo.tsx
+++ src/components/FooterInfo.tsx
@@ -6,12 +6,12 @@ export default function FooterInfo() {
       <div className="-mb-3 flex flex-col items-center max-lg:w-full max-md:-ml-5">
         <Image
           className="max-lg:ml-6 xl:-ml-10"
-          src={'/CLICK SPEED VERTICAL@4x 1.png'}
+          src={'/CLICK SPEED VERTICAL.png'}
           alt="clickspeed logo white"
-          width={250}
+          width={200}
           height={167}
         />
-        <p className="-mt-7 w-60 font-semibold max-lg:ml-10 max-lg:text-center max-md:w-full xl:ml-3">
+        <p className=" w-60 font-semibold max-lg:ml-10 max-lg:text-center max-md:w-full xl:ml-3">
           A internet que se garante.
         </p>
       </div>
