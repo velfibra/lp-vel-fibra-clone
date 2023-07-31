7ec9c00986da2ee6df333e87f5a48a8fb1cdfc6a
Click-Speed <development@clickspeed.net.br>
2023-07-31 14:29:50 -0300

    add : two news faqs in faqComponent

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -38,4 +38,14 @@ const texts = [
     answer:
       'A ClickSpeed é um provedor de internet que se orgulha de oferecer uma conexão estável e de alta velocidade, ideal para jogos online. Nossa infraestrutura de rede avançada e tecnologia de ponta garantem baixa latência e velocidades rápidas de download e upload, proporcionando uma experiência suave e sem interrupções para os jogadores',
   },
+  {
+    question: 'Como posso contratar a Click Speed?',
+    answer:
+      'Você pode contratar a Click Speed direto pelo site, basta escolher o seu plano acima e clicar em ‘Contrate agora’. Você também pode contratar pelo número (91)3085-3190.',
+  },
+  {
+    question: 'Onde posso ter suporte da Click Speed?',
+    answer:
+      'Você poderá falar com um de nossos especialistas através do WhatsApp (91)3085-3190, por direct em nossas redes sociais ou realizar seu auto atendimento na Área do Cliente neste site ou através do aplicativo da Click Speed.',
+  },
 ];
