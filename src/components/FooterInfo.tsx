6629ef7085ab172fe121293a92c9e2917f586cbe
Click-Speed <development@clickspeed.net.br>
2023-08-24 16:55:19 -0300

    style: margin correct

--- src/components/FooterInfo.tsx
+++ src/components/FooterInfo.tsx
@@ -5,7 +5,7 @@ export default function FooterInfo() {
     <>
       <div className="-mb-3 flex flex-col items-center max-lg:w-full max-md:-ml-5">
         <Image
-          className="max-lg:ml-6 xl:-ml-10"
+          className="max-lg:ml-6 xl:-ml-10 xl:-mt-6"
           src={'/LOGO_VERTICAL.png'}
           alt="clickspeed logo white"
           width={200}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ import MobileMenu from './MobileMenu';
 export default function Header() {
   return (
     <header className="top-0 flex h-24 items-center bg-primary max-lg:fixed max-lg:z-10 max-lg:h-20 max-lg:w-full">
-      <Logo className="ml-5 max-lg:hidden" />
+      <Logo className="ml-20 max-lg:hidden" />
       <LogoMobile className="ml-28 lg:hidden" />
       <div className="ml-64 flex w-3/5 justify-between text-white max-2xl:ml-0 max-2xl:w-4/5 max-2xl:justify-evenly max-lg:hidden">
         <nav>
-          <ul className="flex text-2xl font-bold max-2xl:text-xl">
+          <ul className="mt-2 flex text-2xl font-bold max-2xl:text-xl">
             {links.map(({ link, name }) => (
               <li className="ml-16 opacity-75 hover:opacity-100" key={name}>
                 <a href={link}>{name}</a>
