074010c50d17aea08fe1ddaad2bb5b4b4f08d0b8
Click-Speed <development@clickspeed.net.br>
2023-08-28 15:35:26 -0300

    feat: margin for titles

--- src/components/SVAComponent.tsx
+++ src/components/SVAComponent.tsx
@@ -4,9 +4,9 @@ import { CheckCircleIcon } from '@heroicons/react/24/solid';
 export default function SVAComponent() {
   return (
     <section className="mx-auto text-white">
-      <div className="mx-auto flex w-[80%] items-center max-lg:flex-col">
+      <div className="mx-auto flex items-center max-lg:w-[95%] max-lg:flex-col xl:w-[80%]">
         <div className="mx-auto flex flex-col max-lg:mt-5 xl:mt-20 xl:w-[30%]">
-          <h1 className="mb-5 text-left text-5xl font-bold text-white text-shadow max-lg:text-2xl">
+          <h1 className="mb-5 text-left text-5xl font-bold text-white text-shadow max-lg:text-center max-lg:text-2xl">
             Viva a experiência de ter as vantagens da Vel Fibra
           </h1>
           <div className=" mt-2 flex flex-col gap-5 text-xl max-lg:my-10">
@@ -24,7 +24,9 @@ export default function SVAComponent() {
                 </li>
               ))}
             </ul>
-            <p className="font-bold">A Vel Fibra é a internet que coloca você em primeiro lugar!</p>
+            <p className="font-bold max-lg:text-center">
+              A Vel Fibra é a internet que coloca você em primeiro lugar!
+            </p>
           </div>
         </div>
         <div className="rise animate-fadeIn animation-delay-300 xl:w-1/2">
