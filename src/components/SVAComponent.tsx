583f5f5cce13b2d73bc149ea37a905945b772345
Click-Speed <development@clickspeed.net.br>
2023-08-28 15:37:19 -0300

    style: texts

--- src/components/SVAComponent.tsx
+++ src/components/SVAComponent.tsx
@@ -6,10 +6,10 @@ export default function SVAComponent() {
     <section className="mx-auto text-white">
       <div className="mx-auto flex items-center max-lg:w-[95%] max-lg:flex-col xl:w-[80%]">
         <div className="mx-auto flex flex-col max-lg:mt-5 xl:mt-20 xl:w-[30%]">
-          <h1 className="mb-5 text-left text-5xl font-bold text-white text-shadow max-lg:text-center max-lg:text-2xl">
+          <h1 className="text-left text-5xl font-bold text-white text-shadow max-lg:text-center max-lg:text-2xl xl:mb-5">
             Viva a experiência de ter as vantagens da Vel Fibra
           </h1>
-          <div className=" mt-2 flex flex-col gap-5 text-xl max-lg:my-10">
+          <div className=" mt-2 flex flex-col gap-5 max-lg:my-10 xl:text-xl">
             <p>
               Tenha a melhor experiencia da conexão fibra óptica de Belém dentro sa sua casa, Seja
               para filmes, séries, trabalho ou para game, e conte com todos os benefícios como:
@@ -24,7 +24,7 @@ export default function SVAComponent() {
                 </li>
               ))}
             </ul>
-            <p className="font-bold max-lg:text-center">
+            <p className="font-bold max-lg:text-center max-lg:text-xl">
               A Vel Fibra é a internet que coloca você em primeiro lugar!
             </p>
           </div>
