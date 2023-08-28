2b46b1f95745322c23a919b1c37566900cf43bd7
Click-Speed <development@clickspeed.net.br>
2023-08-28 15:57:58 -0300

    add : logo vel

--- src/components/SucessfullyMessage.tsx
+++ src/components/SucessfullyMessage.tsx
@@ -2,7 +2,12 @@ import Image from 'next/image';
 export default function SuceffullyMessage() {
   return (
     <div className="my-10 flex flex-col items-center gap-5 text-center text-lg font-semibold">
-      <Image src={'/CLICK SPEED VERTICAL.png'} alt="logo da clickSpeed" width={200} height={200} />
+      <Image
+        src={'/VEL-FIBRA-VERTICAL-LETTERING-BRANCO 1 (2).png'}
+        alt="logo da Vel"
+        width={200}
+        height={200}
+      />
       <p className="max-lg:w-[300px]">
         Sua solicitação foi enviada com sucesso, em breve um dos nossos consultores irá entrar em
         contato com você,
