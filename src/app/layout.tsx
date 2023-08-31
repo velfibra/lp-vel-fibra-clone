faa4b7767721071e48deab65707e46128314b302
Click-Speed <development@clickspeed.net.br>
2023-08-31 14:27:13 -0300

    refact: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const figtree = Figtree({ subsets: ['latin'] });
 export const metadata = {
   title: 'Vel Fibra – Tenha todo o poder da internet 100% Fibra Óptica',
   description:
-    'Navegue com velocidade máxima e confiabilidade coma o a internet da Click Speed. Internet Click Speed 100% Fibra Óptica para você navegar à vontade. Ultra velocidade de 600mb.',
+    'Navegue com velocidade máxima e confiabilidade com a internet da Vel Fibra. Internet Vel Fibra 100% Fibra Óptica para você navegar à vontade. Ultra velocidade de 650mb.',
   icons: [{ url: '/favicon.ico', sizes: 'any' }],
 };
 
