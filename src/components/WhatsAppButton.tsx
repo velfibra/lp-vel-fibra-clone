664521f7549f52b66c02a3f32af20392fceea818
Click-Speed <development@clickspeed.net.br>
2023-09-15 11:12:53 -0300

    style: whatsappButton

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -27,11 +27,14 @@ export default function WhatsAppButton() {
     // </div>
     <a
       href={`https://wa.me/${phoneNumber}`}
-      className="hover: fixed bottom-3 right-0.5 z-50 text-[#40c351] duration-200 hover:scale-125 hover:text-[#29ff45]"
+      className="hover: fixed bottom-3 right-3 z-50 flex animate-bounce flex-col items-center text-[#40c351] duration-200 hover:scale-125 hover:text-[#29ff45] max-lg:bottom-5 max-lg:right-3"
       target="_blank"
       aria-label="WhatsApp button"
       title="WhatsApp"
     >
+      <p className="rounded-full bg-white p-2 text-lg font-semibold text-black brightness-200">
+        Contrate Aqui!
+      </p>
       <WhatsAppIcon />
     </a>
   );
