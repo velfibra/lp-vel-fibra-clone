b662dacb8c354c2ebd99f8b8e20afe4cdaf9bb54
Click-Speed <development@clickspeed.net.br>
2023-09-22 16:22:52 -0300

    style: pdf full screen

--- src/app/politica-de-privacidade/page.tsx
+++ src/app/politica-de-privacidade/page.tsx
@@ -2,11 +2,11 @@ export default function pdf() {
   return (
     <div className="w-screen">
       <embed
-        className="w-full"
+        className=" min-h-screen w-full"
         src="/politica-de-privacidade.pdf"
         type="application/pdf"
         width="100%"
-        height="800px"
+        height="900px"
       />
     </div>
   );
